76b256e98c3dade2ae7593740b84d58604a4a118
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-09-19 08:46:31 -0300

    feat(config): adiciona opções principais e inicializa carregamento de módulo options
    - Configura opções básicas do Neovim incluindo números de linha,
    comportamento de divisão e indentação
    - Configura tecla líder e desativa espaço nos modos normal e visual
    - Desativa integração com o mouse
    - Configura carregamento de módulos para a configuração principal no
    init.lua

---
+++ lua/core/options.lua
@@ -0,0 +1,25 @@
+local g = vim.g
+local set = vim.keymap.set
+
+g.mapleader = " "
+g.localleader = " "
+set({ "n", "v"}, "<Space>", "<Nop>", { silent = true})
+
+local o = vim.opt
+
+o.number = true
+o.relativenumber = true
+o.splitbelow = true
+o.splitright = true
+
+o.wrap = false
+
+o.expandtab = true
+o.tabstop = 4
+o.shiftwidth = 4
+o.clipboard = "unnamedplus"
+
+o.scrolloff = 999
+vim.opt.cursorline = true
+
+o.mouse=""
