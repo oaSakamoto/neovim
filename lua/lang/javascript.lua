f52e2c2f0e37751e27bf98042b2b1ad4a3df2940
Olimpio Almeida Sakamoto <oasakamoto@proton.me>
2024-09-23 13:37:49 -0300

    fix: muda tsserver para ts_ls
    - tsserver não é mais o nome correto o correto e ts_ls

--- lua/lang/javascript.lua
+++ lua/lang/javascript.lua
@@ -10,14 +10,14 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		lazy = false,
 		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "tsserver" })
+			vim.list_extend(opts.ensure_installed, { "ts_ls" })
 			local capabilities = vim.lsp.protocol.make_client_capabilities()
 			capabilities = vim.tbl_deep_extend("force", capabilities, require("cmp_nvim_lsp").default_capabilities())
 
 			opts.handlers = vim.tbl_deep_extend("force", opts.handlers, {
-				["tsserver"] = function()
+				["ts_ls"] = function()
 					local lspconfig = require("lspconfig")
-					lspconfig.tsserver.setup({
+					lspconfig.ts_ls.setup({
 						capabilities = capabilities,
 						settings = {
 							completions = {
